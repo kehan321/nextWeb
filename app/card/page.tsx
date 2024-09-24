66924eb1d632b76487ef357bcbdb428718a3b256
kehan321 <skyrrah999@gmail.com>
2024-09-25 03:44:08 +0500

    \

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,5 +1,5 @@
+// app/card/Card.tsx
 import React from 'react';
-
 
 interface CardProps {
   imageSrc: string;
@@ -9,13 +9,11 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ imageSrc, title, description }) => {
   return (
-    <div className="bg-white ">
+    <div className="bg-white">
       <img src={imageSrc} alt={title} className="w-full h-72 object-cover" />
       <div className="p-4 flex flex-col justify-center items-center">
         <h2 className="text-xl font-semibold mb-2">{title}</h2>
         <div className="h-1 w-9 bg-blue-500 mb-4"></div>
-
-
         <p className="text-gray-600 text-center">{description}</p>
       </div>
     </div>
